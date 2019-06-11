5044211390962bc58e19c9e588e25bded9a91537
nathanael.ries <nathanael.ries@gmail.com>
2019-06-10 20:54:32 -0500

    Update nolockAssemblies.ps1

--- nolockAssemblies.ps1
+++ nolockAssemblies.ps1
@@ -1,12 +1,13 @@
 $Assemblies = @(
-'System.Security.Principal'
+'System.Collections.Generic',
+'System.Linq',
+'System.Net',
+'System.Net.NetworkInformation',
+'System.Net.Sockets'
 )
 
 foreach($Assembly in $Assemblies){
-
-$ScriptBlock = {[System.Reflection.Assembly]::LoadWithPartialName($Assembly).Location}
-
-$bytes = [System.IO.File]::ReadAllBytes((Invoke-Command -ScriptBlock $ScriptBlock))
-[System.Reflection.Assembly]::Load($bytes)
-
+    $ScriptBlock = {[System.Reflection.Assembly]::LoadWithPartialName($Assembly).Location}
+    $bytes = [System.IO.File]::ReadAllBytes((Invoke-Command -ScriptBlock $ScriptBlock))
+    [System.Reflection.Assembly]::Load($bytes)
 }
